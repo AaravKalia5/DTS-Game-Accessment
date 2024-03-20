d26a9a85fd9e28ce7cf0df9754fd9811236d54a7
Aarav Kalia <21202@wc.school.nz>
2024-03-21 11:09:12 +1300

    21/03/24, I have added the map and the first mini game, but none finished.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import random
 
+money = 0
+inv = []
 
 def intro():
     print("------------------------")
@@ -15,7 +17,7 @@ def intro():
         print("")
 
         if skip == "S":
-            na()
+            map()
             break
 
         elif skip == "A":
@@ -26,7 +28,11 @@ def intro():
 
         else:
             print("")
+            print("")
+            print("---------------------")
             print("Please use 'A' or 'S'")
+            print("---------------------")
+            print("")
             print("")
 
 
@@ -47,11 +53,98 @@ def context():
     print("")
     print("You are currently standing at Te-Papa and there are 5 mini games in total.")
     print("")
-    user_input = input("Press 'R' to read again or press 'A' to continue :  ")
 
 
-def na():
-    print("**Other thing go here**")
+
+    while True:
+        user_input = input("Press 'R' to read again or press 'A' to continue :  ").upper()
+
+        if user_input == "R":
+            context()
+            break
+
+        elif user_input == "A":
+            map()
+            break
+
+        else:
+            print("")
+            print("")
+            print("---------------------")
+            print("Please use 'R' or 'A'")
+            print("---------------------")
+            print("")
+            print("")
+def map():
+    print("")
+    print("")
+    print("This is the map:")
+    print("")
+    print("-------")
+    print("|  A  |  Area 'A' is an 'RPG Fighter'")
+    print("-------")
+    print("")
+    print("-------")
+    print("|  B  |  Area 'B' is ")
+    print("-------")
+    print("")
+    print("-------")
+    print("|  C  |  Area 'C' is")
+    print("-------")
+    print("")
+    print("-------")
+    print("|  D  |  Area 'D' is")
+    print("-------")
+    print("")
+    print("-------")
+    print("|  E  |  Area 'E' is")
+    print("-------")
 
 
+    while True:
+        user_input = input("Enter here: ").upper()
+
+        if user_input == "A":
+            rpg()
+            break
+
+        elif user_input == "B":
+            print("")
+            print("")
+
+
+#RPG SHOOTER
+def rpg():
+    print("")
+    print("")
+    print("Welcome to the RPG Shooter")
+    print("")
+    print("Each battle is worth $50")
+    print("")
+    print("If you defeat a Level 2 Boss You will gain an additional $100")
+    print("")
+
+    while True:
+        user_input = input("Press A to start: ").upper()
+
+        if user_input == "A":
+            rpg_continued()
+            break
+
+        else:
+            print("")
+            print("")
+            print("-------------------------")
+            print("Please Enter 'A' to Start")
+            print("-------------------------")
+            print("")
+            print("")
+
+def rpg_continued():
+    print("")
+    print("")
+    print("")
+
+
+#MAIN
 intro()
